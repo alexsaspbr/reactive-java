f9d4357fbbc7d27512d03a73a95c9f4f7e8794e4
Alex Araujo <alexsaspbr@gmail.com>
2023-08-28 21:55:25 -0300

    aula de rxjava

--- src/main/java/br/com/ada/Main.java
+++ src/main/java/br/com/ada/Main.java
@@ -1,31 +1,100 @@
 package br.com.ada;
 
-import br.com.ada.observer.ClientSubscriber;
-import br.com.ada.observer.LogSubscriber;
-import br.com.ada.observer.Publisher;
-import br.com.ada.shop.Shop;
+import rx.Observable;
+import rx.Observer;
+import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<String> messages = Arrays.asList("Lava louças 8 Serviços 80% de desconto");
+        //Observable - [0...N]
+        //Single     - [0...1]
 
-        Publisher publisher = new Publisher();
-        ClientSubscriber clientSubscriber = new ClientSubscriber("Alex Araujo");
-        ClientSubscriber clientSubscriber2 = new ClientSubscriber("Arthur");
-        ClientSubscriber clientSubscriber3 = new ClientSubscriber("Marcelo");
-        ClientSubscriber clientSubscriber4 = new ClientSubscriber("Thiane");
-        LogSubscriber logSubscriber = new LogSubscriber();
-        publisher.subscribe(clientSubscriber);
-        publisher.subscribe(clientSubscriber2);
-        publisher.subscribe(clientSubscriber3);
-        publisher.subscribe(clientSubscriber4);
-        publisher.subscribe(logSubscriber);
-        Shop shop = new Shop(publisher);
-        shop.sendPromotionMessages(messages);
+        /*Single<String> olaMundo = Single.just("Ola mundo");
+        olaMundo = olaMundo.map(s -> s.concat(", Alex Araujo"));
+        olaMundo.subscribe(System.out::println);*/
 
+   StringBuilder sb = new StringBuilder();
+
+        Observable<String> lettles = Observable.from(new String[]{"O", "a"});
+        //lettles = lettles.toBlocking();
+        lettles
+                .map(l -> l.toUpperCase())
+                //.observeOn(Schedulers.newThread())
+                .doOnCompleted(System.out::println)
+                .subscribe(emailSubscribe());
+
+        System.out.println(sb.toString());
+
+        /*PublishSubject<String> publishSubject = PublishSubject.create();
+        //publishSubject.subscribeOn(Schedulers.newThread());
+        publishSubject.subscribe(emailSubscribe());
+        publishSubject.subscribe(logSubscriber());
+        //publishSubject.onNext("O");
+        //publishSubject.onNext("l");
+        //publishSubject.onNext("a");
+        //publishSubject.onNext(null);
+        publishSubject.onCompleted();*/
+
+    }
+
+    private static Observer<String> emailSubscribe() {
+
+        StringBuilder sb = new StringBuilder();
+
+        return new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                System.out.printf("\nEnviando e-mails para clientes %s \n", sb.toString());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.err.printf("\nOcorreu um erro %s", throwable.getMessage());
+            }
+
+            @Override
+            public void onNext(String s) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                //System.out.println(Thread.currentThread().getName());
+                if(!"l".equalsIgnoreCase(s)) {
+                    System.out.println(s);
+                    sb.append(s);
+                } else {
+                    throw new NullPointerException();
+                }
+            }
+        };
+    }
+
+    private static Observer<String> logSubscriber() {
+        return new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Enviando logs para Elasticsearch");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.err.printf("\nOcorreu um erro %s", throwable.getMessage());
+            }
+
+            @Override
+            public void onNext(String s) {
+                //System.out.println(Thread.currentThread().getName());
+                if(Objects.nonNull(s)) {
+                    System.out.println(s + " Second Observer\n");
+                } else {
+                    throw new NullPointerException();
+                }
+            }
+        };
     }
 }
